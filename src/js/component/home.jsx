fe4b85b414daf9652fa1bfb2db3b8ffe71cc3c2e
fabianchs <fabichasola@hotmail.com>
2021-07-29 21:32:44 +0000

    working con responsive creator

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,6 +10,49 @@ const Home = () => {
 	const [price, setPrice] = useState(["0"]);
 
 	const [renderedEditor, setRenderedEditor] = useState("");
+	const [renderedInvoice, setRenderedInvoice] = useState("");
+
+	const p_style = "m-0 p-0 d-flex justify-content-start";
+
+	function createInvoice() {
+		let list_invoice = product.map((element, index) => (
+			<div key={index} className="container-fluid">
+				<p className={p_style}>{element.toUpperCase()}</p>
+				<p className={p_style}>
+					CANTIDAD: &nbsp;
+					{amount[index]}
+				</p>
+				<p className={p_style}>
+					PRECIO UNITARIO: &nbsp; &#8353;{price[index]}
+				</p>
+				<p>&nbsp;</p>
+			</div>
+		));
+
+		let counter = 0;
+
+		for (let i = 0; i < product.length; i++) {
+			counter = counter + parseFloat(amount[i]) * parseFloat(price[i]);
+		}
+
+		let final = [counter.toFixed(2)];
+
+		let final_price = (
+			<div className="container-fluid">
+				<p className={p_style}>TOTAL: &nbsp; &#8353;{final}</p>
+			</div>
+		);
+
+		list_invoice.push(final_price);
+		list_invoice.unshift(
+			<div className="container-fluid">
+				<p className={p_style}>FACTURA PROFORMA</p>
+				<p className={p_style}>&nbsp;</p>
+			</div>
+		);
+
+		setRenderedInvoice(list_invoice);
+	}
 
 	function editProduct(e, index) {
 		let aux_product = product;
@@ -21,18 +64,30 @@ const Home = () => {
 		createInputs();
 	}
 	function editAmount(e, index) {
-		//
+		let aux_amount = amount;
+
+		aux_amount[index] = e.target.value;
+
+		setAmount(aux_amount);
+
+		createInputs();
 	}
 	function editPrice(e, index) {
-		//
+		let aux_price = price;
+
+		aux_price[index] = e.target.value;
+
+		setPrice(aux_price);
+
+		createInputs();
 	}
 
 	function addToArrays() {
 		let elements = [product, amount, price];
 
 		elements[0].push("");
-		elements[1].push("0");
-		elements[2].push("1");
+		elements[1].push("1");
+		elements[2].push("0");
 
 		setProduct(elements[0]);
 		setAmount(elements[1]);
@@ -44,9 +99,11 @@ const Home = () => {
 	function deleteFromArrays() {
 		let elements = [product, amount, price];
 
-		elements[0].splice(-1, 1);
-		elements[1].splice(-1, 1);
-		elements[2].splice(-1, 1);
+		if (product.length > 1) {
+			elements[0].splice(-1, 1);
+			elements[1].splice(-1, 1);
+			elements[2].splice(-1, 1);
+		}
 
 		setProduct(elements[0]);
 		setAmount(elements[1]);
@@ -67,6 +124,7 @@ const Home = () => {
 						placeholder="Producto"
 						className="border-dark bg-transparent"
 						defaultValue={element}
+						onChange={() => editProduct(event, index)}
 					/>
 				</div>
 				<div className="col-3 p-1">
@@ -76,6 +134,7 @@ const Home = () => {
 						placeholder="Cantidad"
 						className="border-dark bg-transparent"
 						defaultValue={amount[index]}
+						onChange={() => editAmount(event, index)}
 					/>
 				</div>
 				<div className="col-3 p-1">
@@ -85,6 +144,7 @@ const Home = () => {
 						placeholder="Precio Unitario"
 						className="border-dark bg-transparent"
 						defaultValue={price[index]}
+						onChange={() => editPrice(event, index)}
 					/>
 				</div>
 			</div>
@@ -116,7 +176,7 @@ const Home = () => {
 										{" "}
 										<button
 											className="btn btn-secondary"
-											onClick={() => addToArrays()}>
+											onClick={() => createInvoice()}>
 											Generar
 										</button>
 									</span>
@@ -138,7 +198,7 @@ const Home = () => {
 								</div>
 							</div>
 						</div>
-						<div className="row col-6"></div>
+						<div className="row col-6">{renderedInvoice}</div>
 					</div>
 				</div>
 			</div>
